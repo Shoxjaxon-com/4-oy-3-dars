9b8ac88b46b337fbb8496be50647de283c6263ca
Shoxjaxon-com <alijonovshohjahon03@gmail.com>
2024-07-19 10:25:01 -0700

    vazifa

---
+++ index.js
@@ -0,0 +1,211 @@
+// IF masalalar
+
+ // 1-masala
+// let yosh =+prompt("Yoshingizni kirirting")
+// let res = "";
+// if( yosh >= 18){
+//     res = "Siz katta yoshdagisiz"
+//     console.log(res);
+// }else{
+//     res = "Siz hali yoshsiz"
+//     console.log(res);
+// }
+
+// 2-masala
+
+// let a =+prompt("Sonni kirirting")
+// let res;
+// if ( a% 2== 0){
+//  res = a
+//  console.log( "Juft son" + res);
+// }
+// if ( a%2==1){
+//     res =a ;
+//     console.log("Toq son" + res);
+// }
+
+// 3-masala
+
+// let a =+prompt("Sonni kiriting")
+
+// let res ;
+
+// if ( a >0){
+//     res = a;
+//     console.log("Musbat son" + res);
+// }
+
+// if ( a < 0) {
+//     res =a ;
+//     console.log("Manfiy son" + res);
+// }
+
+// 4-masala
+//  let harorat =+prompt( "Haroratni kiriting")
+//  let res ;
+// if ( harorat >= 20){
+//     res = harorat;
+//     console.log("Yengil kiying" + res);
+// }
+// if ( harorat <= 20){
+//     res =harorat;
+//     console.log("Qalin kiying" + res);
+// }
+
+// 5-masala
+// let ball =+prompt("Toplangan balni kiriting")
+
+// let alo ;
+// let yaxshi ;
+// let qoniqarli;
+// let yomon;
+
+// if( ball >= 90){
+//     alo = 90 ;
+//     console.log("Alo" +alo);
+// }
+// if ( (ball >= 89) || (ball >= 70)){
+//     yaxshi = 89;
+//     yaxshi = 70 ;
+//     console.log("Yaxshi");
+// }
+
+// if ( (ball>= 69) || (ball >= 50)){
+//     qoniqarli = 69;
+//     qoniqarli = 50;
+//     console.log("Qoniqarli");
+// }
+
+// if ( ball <= 50){
+//     yomon = 50;
+//     console.log("Yomon");
+// }
+
+// 6-masala
+// let mehmon =prompt("Mehmonni ismini kiriting")
+// let res ;
+// if( mehmon === 'Ali'){
+//  res = 'Salom  Ali'
+// }else{
+//     res= 'Salom mehmon'
+// }
+// console.log(res);
+
+// 7-masala
+// let narx =+prompt("Summani kiriting")
+// let res;
+// if( narx >= 100){
+//     res = "Chegirma qo'llanildi"
+// }else{
+//     res = "Chegirma mavjud emas"
+// }
+// console.log(res);
+
+
+// Ternary 
+
+// 1-masala
+// let harorat =+prompt("Haroratni kirirting")
+
+// harorat > 25 ? console.log("Issiq"): console.log("Sovuq");  
+
+// 2-masala
+// let ball =+prompt("Ballarni kiriting")
+
+// ball >= 50 ? console.log("Otdi"): console.log("Yiqildi");
+
+// 3-masala
+// let son =+prompt("Sonni kiriting")
+
+// son > 0 ? console.log("Musbat"): console.log("Manfiy");
+
+// For 
+
+// 1-masala
+// for (let i= 1; i <= 10 ; i ++) {
+//     console.log(i);
+// }
+
+// 2-masala
+// for (let i= 10; i >= 1 ; i --) {
+//     console.log(i);
+// }
+
+// 3-masala
+// for( let i=1; i <= 100;  i ++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+// 4-masala
+    // for( let i=1 ; i <= 100 ; i ++){
+    //     if ( i%2 ==1){
+    //         console.log(i);
+    //     }
+    // }
+// 5-masala
+// let sum ;
+// for( let i=1 ; i <=10 ; i++){
+//     sum = sum+i;
+// }
+// console.log(sum);
+
+    // 6-masala
+    // let sum;
+    // for( let i=1 ; i<=10; i++){
+    //     sum = sum *i;
+    // }
+    // console.log(sum);
+
+    // 8-masala
+    // for( let i =1 ; i <= 50; i++){
+    //     console.log(i * i);
+    // }
+
+    // 9-masala
+    //     for( let i =1 ; i <= 20; i++){
+    //     console.log(i * i *i);
+    // }
+    // 10-masala
+    // let sum;
+    // for( let i = 1; i <=100 ; i++){
+    //     sum = sum + i;
+    //     console.log(sum);
+    // }
+
+    // 11-masala
+    // for (let i = 0; i < 5; i++) {
+    //     console.log("Salom");
+    // }
+
+    // 12-masala
+    // for (let i = 1; i <= 100; i++) {
+    //     console.log(i * 2);
+    // }  
+
+    // 13-masala
+    // for (let i = 1; i <= 10; i++) {
+    //     console.log(Math.pow(2, i));
+    // }
+
+    // 14-masala
+    // let num =+prompt("Sonni kiriting")
+    // for( let i = 1 ; i <=num; i++){
+    //     console.log(Math.pow(num, i));
+    // }
+
+
+
+    // Boolean
+    // 1-masala
+    let a =+prompt("Sonni kiriting")
+    let b =+prompt("Sonni kiriting")
+    let juft;
+    let Toq;
+    if( (a%2==1 && a%2==0) || (b%2==1 && b%2==0)){
+        console.log(juft + Toq);
+    }
+
+      
+    
